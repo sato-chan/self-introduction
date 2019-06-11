0c983ea6b187d05fff4fd751d883eb02df554a94
佐藤ゆうこ <black.black.enlightment@gmail.com>
2019-06-11 09:56:57 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bolt - Bootstrap Agency Template</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/favicon.png" rel="icon">
+  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900|Raleway:400,300,700,900" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Template Name: Bolt
+    Template URL: https://templatemag.com/bolt-bootstrap-agency-template/
+    Author: TemplateMag.com
+    License: https://templatemag.com/license/
+  ======================================================= -->
+</head>
+
+<body data-spy="scroll" data-offset="80" data-target="#thenavbar">
+
+  <!-- Fixed navbar -->
+  <div id="thenavbar" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        <a class="navbar-brand" href="#"><i class="fa fa-bolt"></i></a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="active"><a href="#hello" class="smoothscroll">Home</a></li>
+          <li><a href="#green" class="smoothscroll">About</a></li>
+          <li><a href="#portfolio" class="smoothscroll">Portfolio</a></li>
+          <li><a href="#contact" class="smoothscroll">Contact</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+    </div>
+  </div>
+
+  <div id="hello">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 centered">
+        <div class="img/IMG_0121.jpg">
+         <h1>Sato Yuko</h1>
+        <br>
+        <h3>プログラミング、留学経験初体験</h3>
+        <h3>三重県の田舎出身、元2年間先生</h3>
+        <h3>仕事うまくいかなくて自由になりたくて家出してきました</h3>
+        </div>
+      </br>
+
+        </div>
+        
+         <div class="float_test1">
+
+            <h3>hobby</h3>　
+
+            <h3>音楽(Mr.Chilren,Beyonceなどなど)</h3>
+            <h3>たまに読書(ものの見方検定)</h3>
+            <h3>衝動買い</h3>
+            <img src="img/IMG_0008.jpg">
+
+        </div>  
+        <div>
+            <!-- 文字列をわける -->
+        <div class="float_test2">
+          <div>
+            <h3>things I want</h3>
+            <h3>ベンツ</h3>
+            <h3>ポジティブ思考</h3> 
+            <h3>使える英語能力</h3> 
+            <img src="img/IMG_0142.jpg">  
+
+            </div>
+          </div>
+        </div>
+         <div>
+          <div class="float_test3">
+            <h3>my favorite food</h3>
+            <h3>餃子</h3>
+            <h3>ラーメン</h3>
+            <h3>だしの効いた味噌汁</h3>
+            <img src="img/IMG_0141.jpg">
+       
+        
+      </div>
+        </div>
+        <!-- /col-lg-8 -->
+      </div>
+      <!-- /row -->
+    </div>
+    <!-- /container -->
+  </div>
+  <!-- /hello -->
+
+  <div id="green">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 centered">
+          <img src="img/IMG_0086.jpg" alt="">
+        </div>
+
+        <div class="col-lg-7 centered">
+         <h3>What I want to do in the future</h3>
+          <p> 1. 英語をペラペラに話せる(2年後)</p>
+          <p> 2. 何事もポジティブに考えれる(2年後)</p>
+          <p> 3. 海外で仕事をする(1年後)</p>
+          <p> 4. 自分の能力で自分のやりたいことができる給料を稼げるようになる(7年後)</p>
+          <p> 5.ベンツに乗る(7年後)</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="portfolio"></div>
+  <div class="container">
+    <div class="row centered mt grid">
+       <h3>my favorite photos</h3>
+      <div class="mt"></div>
+      <div class="col-lg-4">
+        <a href="#"><img src="img/IMG_0054.jpeg" alt=""></a>
+      </div>
+
+      <div class="col-lg-4">
+        <a href="#"><img src="img/IMG_0314.jpg" alt="">
+        </a>
+      </div>
+
+      <div class="col-lg-4">
+        <a href="#"><img src="img/IMG_0322.jpg" alt=""></a>
+      </div>
+    </div>
+
+    <div class="row centered mt grid">
+      <div class="mt"></div>
+      <div class="col-lg-4">
+        <a href="#"><img src="img/IMG_0348.jpg" alt=""></a>
+      </div>
+      <div class="col-lg-4">
+        <a href="#"><img src="img/IMG_0578.jpg" alt=""></a>
+      </div>
+      <div class="col-lg-4">
+      <!-- 画像を反転させる方法 -->
+        <a href="#"><img src="img/IMG_0068.jpeg" style="transform:rotate(+90deg);" alt="">
+        </a>
+     
+      </div>
+    </div>
+
+    <div class="row mt centered">
+      <div class="col-lg-7 col-lg-offset-1 mt">
+        <p class="lead">感謝している人。辛い時、うまくいかない時にたくさん支えてくれた人。</p>
+      </div>
+
+      <div class="col-lg-3 mt">
+        <p><button type="button" class="btn btn-theme btn-lg">Email Me!</button></p>
+      </div>
+    </div>
+  </div>
+
+
+ 
+    
+
+  <div id="copyrights">
+    <div class="container">
+      <p>
+        &copy; Copyrights <strong>Bolt</strong>. All Rights Reserved
+      </p>
+      <div class="credits">
+        <!--
+          You are NOT allowed to delete the credit link to TemplateMag with free version.
+          You can delete the credit link only if you bought the pro version.
+          Buy the pro version with working PHP/AJAX contact form: https://templatemag.com/bolt-bootstrap-agency-template/
+          Licensing information: https://templatemag.com/license/
+        -->
+        Created with Bolt template by <a href="https://templatemag.com/">TemplateMag</a>
+      </div>
+    </div>
+  </div>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/php-mail-form/validate.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/chart/chart.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
